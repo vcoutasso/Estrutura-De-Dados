4afc843122d54141ec6a5f14bfeb9967fb59cdf1
Vinicius Couto <vinicouto12@gmail.com>
2019-09-04 10:31:17 -0300

    Lista 1 - alterações em ex3 para melhor ilustrar o funcionamento

--- Prova1/Lista1/ex3.c
+++ Prova1/Lista1/ex3.c
@@ -96,12 +96,11 @@ int main() {
     Queue* q = newQueue();
 
     push(2, q);
-    push(2, q);
-    push(2, q);
-    push(2, q);
-    push(2, q);
-    push(2, q);
-    push(2, q);
+    push(4, q);
+    push(6, q);
+    push(8, q);
+    push(1, q);
+    push(3, q);
     printQueue(q);
 
     return 0;
