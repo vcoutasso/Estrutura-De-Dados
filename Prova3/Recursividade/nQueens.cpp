3ac3ad4425c9798106ac89cd9fcd4c59e66a711f
Vinícius Couto <vinicouto12@gmail.com>
2019-10-09 11:40:12 -0300

    Remoção de funções e variáveis não utilizadas

--- Prova3/Recursividade/nQueens.cpp
+++ Prova3/Recursividade/nQueens.cpp
@@ -16,8 +16,6 @@ struct Queen {
 	int col;
 };
 
-// true if position is attacked and false otherwise
-std::vector<bool> attackedPositions;
 // holds the positions of all placed queens
 std::vector<Queen> queens;
 
@@ -35,59 +33,11 @@ bool isSafe(int row, int col) {
 }
 
 
-void clearBoard() {
-	// reset board
-	std::fill(attackedPositions.begin(), attackedPositions.end(), false);
-}
-
-// sets attackedPositions[k] to true if it is under attack. not used because its too costly
-void updateAttackedPositions(int n, std::vector<Queen>::iterator it) {
-	int col;
-	int row;
-	int queenPosition;
-	int i;
-
-	for (; it != queens.end(); ++it) {
-
-		queenPosition = it->col * n + it->row;
-
-		col = it->col;
-		row = it->row;
-
-		// same row
-		for (i = 0; i < n; ++i)
-			attackedPositions[n * i + row] = true;
-
-		// diagonal
-		for (i = 1; i < n; ++i) {
-			if (queenPosition - i * (n + 1) > 0)
-				attackedPositions[queenPosition - i * (n + 1)] = true;
-			if (queenPosition + i * (n + 1) < n * n)
-				attackedPositions[queenPosition + i * (n + 1)] = true;
-		}
-
-		while (col > 0 && row < n - 1) {
-			++row;
-			--col;
-		}
-
-		while (row >= 0 && col < n) {
-			attackedPositions[col * n + row] = true;
-			--row;
-			++col;
-		}
-
-		attackedPositions[queenPosition] = false;
-	}
-}
-
 int findSolution(int n, int col) {
 	std::vector<int> possiblePositions;
 
 	for (int i = 0; i < n; ++i) {
-		// if it isn't under attack
-		if (!attackedPositions[col * n + i])
-			possiblePositions.push_back(i);
+		possiblePositions.push_back(i);
 	}
 
 	int row = 0;
@@ -96,7 +46,7 @@ int findSolution(int n, int col) {
 	while (true) {
 		do {
 			// No valid position within this column with current queens placement. Return 1 step and keep trying
-			if (possiblePositions.size() == 0)
+			if (possiblePositions.empty())
 				return 0;
 
 			index = rand() % possiblePositions.size();
@@ -138,9 +88,6 @@ void printBoard(int n) {
 }
 
 void nQueens(int n) {
-	// create board
-	attackedPositions.insert(attackedPositions.begin(), n * n, false);
-
 	// find solution for the first column. since its a recursive function, there's no need to call it for the remaining columns
 	findSolution(n, 0);
 
