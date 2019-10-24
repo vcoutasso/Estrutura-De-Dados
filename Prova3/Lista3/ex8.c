df12e29a311a5ced80f0a824ef54bf9fc1c958eb
Vinicius Couto <vinicouto12@gmail.com>
2019-10-24 17:48:48 -0300

    ex8 arrumado

--- Prova3/Lista3/ex8.c
+++ Prova3/Lista3/ex8.c
@@ -12,18 +12,40 @@ struct List {
     List *next;
 };
 
-void insereElemento(List *list, int data) {
-    if (list == NULL) {
-        list = malloc(sizeof(List));
-        list->data = data;
-        list->next = NULL;
+void insereElemento(List **list, int data) {
+    if (*list == NULL) {
+        *list = malloc(sizeof(List));
+        (*list)->data = data;
+        (*list)->next = NULL;
     }
-    else if (list->next == NULL) {
+    else if ((*list)->next == NULL) {
         List *l = malloc(sizeof(List));
         l->data = data;
         l->next = NULL;
-        list->next = l;
+        (*list)->next = l;
     }
     else
-        insereElemento(list->next, data);
+        insereElemento(&(*list)->next, data);
+}
+
+void printList(List *list) {
+    if (list == NULL)
+        printf("Error! List is NULL\n");
+    else {
+        printf("%d ", list->data);
+        if (list->next != NULL)
+            printList(list->next);
+    }
+}
+
+int main() {
+    List **list = malloc(sizeof(List*));
+
+    printList(*list);
+    insereElemento(list, 1);
+    insereElemento(list, 2);
+    insereElemento(list, 3);
+    insereElemento(list, 4);
+    printList(*list);
+
 }
