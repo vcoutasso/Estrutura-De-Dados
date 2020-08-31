7b2b10b0868a04aca424ef4347492f729937dc35
Vinícius Couto <vinicouto12@gmail.com>
2020-08-31 14:37:39 -0300

    Lista 4 de ED2

---
+++ ED2/Lista4/avl.h
@@ -0,0 +1,37 @@
+#ifndef __AVL_H_
+#define __AVL_H_
+
+typedef struct node {
+   char chave;
+   int altura;
+   struct node* esq;
+   struct node* dir;
+} No, Arvore;
+
+int maior (int esq, int dir);
+
+int altura (Arvore* a);
+
+int atualizar_altura (Arvore *a);
+
+int balanceamento (Arvore *a);
+
+Arvore* rotacao_simples_esq (Arvore* a);
+
+Arvore* rotacao_simples_dir (Arvore* a);
+
+Arvore* rotacao_dupla_esq (Arvore* a);
+
+Arvore* rotacao_dupla_dir (Arvore* a);
+
+Arvore* atualizar_fb_dir (Arvore *a);
+
+Arvore* atualizar_fb_esq (Arvore *a);
+
+Arvore* inserir (Arvore *a, char chave);
+
+Arvore* remover (Arvore *a, char chave);
+
+void imprimir_in_order (Arvore* a, int nivel);
+
+#endif // __AVL_H_
